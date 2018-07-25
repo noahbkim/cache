cdd1cfc757aff78656d3fc197615736154f7647a
Noah Kim <noahbkim@gmail.com>
2018-07-25 14:01:17 -0400

    Finish up tests and tweaks

--- cache.py
+++ cache.py
@@ -5,12 +5,15 @@ import time
 import tempfile
 import atexit
 import logging
-from typing import Union, Callable, IO, Dict, Any, AnyStr
+from typing import Union, Callable, IO, Dict, Any, AnyStr, Optional
 from pathlib import Path
 from functools import wraps
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %h:%M:%S %p",
+    format="%(asctime)s %(levelname)s: %(message)s")
 
 
 class Files:
@@ -27,13 +30,9 @@ class Files:
     def __init__(self, root: Path):
         """Initialize a new file manager for a cache."""
 
-        self._root = root
+        self._root = root.absolute()
         self._data = self._root.joinpath(self.DATA)
         self._manifest = self._root.joinpath(self.MANIFEST)
-
-    @property
-    def root(self):
-        return self._root
 
     @contextlib.contextmanager
     def manifest(self, mode: str="r") -> IO:
@@ -45,7 +44,7 @@ class Files:
         except FileNotFoundError:
             logging.debug("no manifest file found!")
             self._root.mkdir(parents=True, exist_ok=True)
-            with self._manifest.open(mode) as file:
+            with self._manifest.open("w") as file:
                 json.dump({}, file)
             logging.debug("manifest created")
             with self._manifest.open(mode) as file:
@@ -75,15 +74,25 @@ class Entry:
     """Entry in a manifest file."""
 
     name: str
+    data: Any
     created: float
     expiration: float
 
-    def __init__(self, name: str, created: float=None, expiration: float=None):
-        """Initialize a new entry with formed values."""
+    def __init__(self, name: str=None, created: float=None, expiration: float=None, **kwargs):
+        """Initialize a new entry with formed values.
+
+        We don't assign the data attribute on initialization because
+        we want to be able to represent None as an actual value that
+        could be cached.
+        """
 
         self.name = name
         self.created = created or time.time()
         self.expiration = expiration or 0
+
+        # Check this way so that None is a valid value
+        if "data" in kwargs:
+            self.data = kwargs.pop("data")
 
     def dump(self) -> Dict[str, str]:
         """Dump an entry to JSON."""
@@ -107,40 +116,20 @@ class Entry:
 class Manifest:
     """Manifest file wrapper."""
 
-    files: Files
+    _files: Files
+    _manifest: Dict[str, Entry]
 
-    def __init__(self, files: Files, sync: bool=True):
-        """Initialize a manifest with the cache file manager.
-
-        The sync argument allows the user to specify whether the
-        corresponding manifest file should be modified every time the
-        manifest object in memory is. If sync is turned off, the
-        manifest file is only modified at exit.
-        """
+    def __init__(self, files: Files):
+        """Initialize a manifest with the cache file manager."""
 
         self._files = files
-        self._sync = sync
         self._manifest = {}
-
-        if not self._sync:
-            self._open()
-
-    def _open(self):
-        """Read the manifest once if sync is disabled."""
-
-        self._manifest = self._read()
-        atexit.register(self._close)
-
-    def _close(self):
-        """Write and close the manifest file if sync is disabled"""
-
-        self._write(self._manifest)
 
     def _read(self) -> dict:
         """Read the manifest file."""
 
         try:
-            with self.files.manifest() as file:
+            with self._files.manifest() as file:
                 data = json.load(file)
         except json.JSONDecodeError:
             data = self.reset()
@@ -149,14 +138,14 @@ class Manifest:
     def _write(self, data: Dict[str, Entry]) -> dict:
         """Write to the manifest file."""
 
-        with self.files.manifest("w") as file:
+        with self._files.manifest("w") as file:
             json.dump(data, file)
         return data
 
-    def get(self, key: str) -> Union[Entry, None]:
+    def get(self, key: str) -> Optional[Entry]:
         """Get a key from the manifest file."""
 
-        result = (self._read() if self._sync else self._manifest).get(key)
+        result = self._read().get(key)
         if result is not None:
             result = Entry.load(result)
         return result
@@ -164,19 +153,17 @@ class Manifest:
     def set(self, key: str, entry: Entry) -> Entry:
         """Set a key in the manifest."""
 
-        data = self._read() if self._sync else self._manifest
+        data = self._read()
         data[key] = entry.dump()
-        if self._sync:
-            self._write(data)
+        self._write(data)
         return entry
 
     def pop(self, key: str) -> Entry:
         """Remove a key and value from the manifest."""
 
-        data = self._read() if self._sync else self._manifest
+        data = self._read()
         entry = Entry.load(data.pop(key))  # Maybe too heavy?
-        if self._sync:
-            self._write(data)
+        self._write(data)
         return entry
 
     def reset(self) -> Dict[str, Entry]:
@@ -210,7 +197,7 @@ def write(data: AnyStr, file: IO):
     file.write(data)
 
 
-def read(file: IO):
+def read(file: IO) -> AnyStr:
     return file.read()
 
 
@@ -220,7 +207,7 @@ StringOrStringCallable = Union[Callable[..., str], str]
 class Cache:
     """A cache object used to speed up access to resources."""
 
-    def __init__(self, inside: Union[str, Path]=None, root: Union[str, Path]=None):
+    def __init__(self, inside: Union[str, Path]=None, root: Union[str, Path]=None, sync: bool=True):
         """Initialize a new cache.
 
         The inside arguments specifies the directory in which the
@@ -228,6 +215,11 @@ class Cache:
         argument specifies the absolute location of cache directory.
         Root takes precedence over inside in the event that a user
         specifies both arguments, although this should not happen.
+
+        The sync argument allows the user to specify whether the
+        corresponding manifest file should be modified every time the
+        manifest object in memory is. If sync is turned off, the
+        manifest file is only modified at exit.
         """
 
         if type(inside) == str:
@@ -247,6 +239,7 @@ class Cache:
 
     def __call__(self,
                  f: Callable=None,
+                 *,
                  serialize: StringOrStringCallable=None,
                  file: StringOrStringCallable=None,
                  extension: StringOrStringCallable=None,
@@ -254,7 +247,7 @@ class Cache:
                  retrieve: Callable[[IO], Any]=None,
                  persist: bool=True,
                  expiration: float=None,
-                 binary: bool=False):
+                 binary: bool=False) -> Callable:
         """Decorate a function and cache the return.
 
         This object primarily acts as a decorator, so to provide that
@@ -269,16 +262,19 @@ class Cache:
         store(path, data) and retrieve(path) respectively, and will
         By default, a standard file write is used.
 
+        The expiration argument should be used to set the number of
+        seconds before the function will be re-invoked.
+
         Binary is the file mode that should be used to open the file
         before it is read from or written to. The w argument as
         opposed to the wb.
         """
 
-        def decorator(func):
+        def decorator(func: Callable) -> Callable:
             """Return the configured decorator."""
 
             @wraps(func)
-            def wrapper(*args, reload=False, **kwargs):
+            def wrapper(*args, reload=False, **kwargs) -> Any:
                 """Add options for memory and file system caching.
 
                 First check if the function object is in the memory
@@ -287,42 +283,58 @@ class Cache:
                 qualified name is listed in it.
                 """
 
-                if func in self._cache and not reload:
-                    return self._cache[func]
+                # Get a unique key from the function and arguments, check if in manifest
+                arguments = "" if serialize is None else call(serialize, *args, **kwargs)
+                key = _serialize(func, arguments)
 
-                path = ""  # Path will be defined, this just prevents code check errors
-                if persist:
-
-                    # Get a unique key from the function and arguments, check if in manifest
-                    arguments = "" if serialize is None else call(serialize, *args, **kwargs)
-                    key = _serialize(func, arguments)
-                    entry = self._manifest.get(key)
+                # Get the entry from memory of the manifest
+                if not reload:
+                    entry = None
+                    if key in self._cache:
+                        entry = self._cache[key]
+                    if persist:
+                        entry = self._manifest.get(key)
 
                     # If it is, get the data
                     if entry is not None:
-                        logging.debug("found path in manifest")
-                        data = self.retrieve(entry.name, method=retrieve, binary=binary)
-                        if data is not None:
-                            logging.debug("retrieved data correctly")
-                            self._cache[func] = data
-                            return data
+                        logging.debug("found entry")
+
+                        # Check if it has expired
+                        if entry.expiration == 0 or time.time() - entry.created < entry.expiration:
+
+                            # Try to get the data from the entry
+                            try:
+                                return entry.data
+                            except AttributeError:
+                                pass
+
+                            # If we're persisting, check the file system
+                            if persist:
+                                try:
+                                    entry.data = self.retrieve(entry.name, method=retrieve, binary=binary)
+                                except (FileNotFoundError, Exception) as e:
+                                    logging.debug("caught {} while retrieving data".format(e))
+                                else:
+                                    return entry.data
+
                         else:
-                            logging.debug("couldn't access cache data")
+                            logging.debug("data has expired")
 
-                    # Otherwise, generate a new path with the provided file function
-                    if file is not None:
-                        path = call(file, *args, **kwargs) + (extension or "")
-                        self._manifest.set(key, Entry(path, expiration=expiration))
-
-                    # Otherwise, generate a random file name
-                    else:
-                        path = self._files.random() + (extension or "")
-                        self._manifest.set(key, Entry(path, expiration=expiration))
-
-                self._cache[func] = result = func(*args, **kwargs)
+                # Set the result and add the entry to the cache
+                result = func(*args, **kwargs)
+                self._cache[key] = entry = Entry(expiration=expiration, data=result)
+                logging.debug("called function")
 
                 if persist:
-                    self.store(path, result, method=store, binary=binary)
+
+                    # Set a name for the entry and store it in the manifest
+                    name = call(file, *args, **kwargs) if file else self._files.random()
+                    entry.name = name + (extension or "")
+                    self._manifest.set(key, entry)
+                    logging.debug("add to manifest")
+
+                    # Write to the file system
+                    self.store(name, result, method=store, binary=binary)
                     logging.debug("stored results")
 
                 return result
@@ -337,14 +349,8 @@ class Cache:
         """Read a file from the cache."""
 
         method = method or read
-        try:
-            with self._files.data(name, "rb" if binary else "r") as file:
-                return method(file)
-        except FileNotFoundError:
-            return None
-        except Exception as exception:
-            logging.error(f"caught {exception} while retrieving")
-            return None
+        with self._files.data(name, "rb" if binary else "r") as file:
+            return method(file)
 
     def store(self, name: str, data, method=None, binary: bool=False):
         """Write data to a file in the cache."""
@@ -352,6 +358,3 @@ class Cache:
         method = method or write
         with self._files.data(name, "wb" if binary else "w") as file:
             method(data, file)
-
-
-cache = Cache()
---
+++ tests.py
@@ -0,0 +1,96 @@
+import unittest
+import json
+import shutil
+import time
+import os
+
+from cache import Cache
+from cache import logging
+
+
+counter = 0
+
+
+def data(argument=None):
+    """Get some arbitrary data."""
+
+    if argument is not None:
+        return argument
+
+    global counter
+    counter += 1
+    return str(counter)
+
+
+cache = Cache()
+
+
+class CacheTest(unittest.TestCase):
+    """Test all cache cases."""
+
+    def tearDown(self):
+        """Clear the memory cache each time."""
+
+        logging.info("clearing memory cache")
+        cache._cache.clear()
+        cache._manifest.reset()
+
+    def test_memory_cache(self):
+        """Check storing a function call in memory."""
+
+        logging.info("starting memory cache test")
+        func = cache(persist=False)(data)  # Essentially calls the decorator
+        result = func()
+        assert func() == result  # The counter should change but the cached result should not
+
+    def test_persistent_cache(self):
+        """Check storing a function call in a file."""
+
+        logging.info("starting persistent cache test")
+        func = cache(persist=True)(data)
+        result = func()
+        cache._cache.clear()
+        assert func() == result
+
+    def test_memory_serialize_arguments(self):
+        """Check custom functions for argument serialization."""
+
+        logging.info("starting argument serialization test")
+        func = cache(persist=False, serialize=lambda argument: str(hash(argument)))(data)
+        result = func(argument="Hello, world!")
+        key = "tests.data({})".format(hash("Hello, world!"))
+        assert cache._cache[key].data == result
+
+    def test_memory_expiration(self):
+        """Test whether expiration works."""
+
+        logging.info("starting expiration cache test")
+        func = cache(persist=False, expiration=2)(data)
+        result = func()
+        time.sleep(1)
+        assert func() == result
+        time.sleep(1.5)
+        assert func() != result
+
+    def test_persistent_file(self):
+        """Check if file names are stored correctly."""
+
+        logging.info("starting file name cache test")
+        func = cache(persist=True, file=lambda argument: str(argument), extension=".txt")(data)
+        func("hello")
+        assert os.path.exists(cache._files._data.joinpath("hello.txt"))
+
+    def test_store_retrieve(self):
+        """Check the store and retrieve overrides."""
+
+        logging.info("starting store and retrieve test")
+        func = cache(persist=True, store=json.dump, retrieve=json.load)(data)
+        func({"number": 1})
+        cache._cache.clear()
+        assert type(func()) == dict and func()["number"] == 1
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up after ourselves."""
+
+        shutil.rmtree(cache._files._root)
