76c7008cb8bde46a6c4f6273b2bd926028d949d0
Noah Kim <noahbkim@gmail.com>
2019-07-24 19:36:02 -0400

    Update tests

--- tests.py
+++ tests.py
@@ -4,7 +4,6 @@ import pickle
 import time
 
 from cache import Cache
-from cache import logging
 
 
 counter = 0
@@ -31,13 +30,11 @@ class CacheTest(unittest.TestCase):
     def tearDown(cls):
         """Clear the memory cache each time."""
 
-        logging.info("clearing memory cache")
         cache._cache.clear()
 
     def test_memory_cache(self):
         """Check storing a function call in memory."""
 
-        logging.info("starting memory cache test")
         func = cache(persist=False)(data)  # Essentially calls the decorator
         result = func()
         self.assertEqual(func(), result)  # The counter should change but the cached result should not
@@ -45,7 +42,6 @@ class CacheTest(unittest.TestCase):
     def test_persistent_cache(self):
         """Check storing a function call in a file."""
 
-        logging.info("starting persistent cache test")
         func = cache(persist=True, store=pickle.dump, retrieve=pickle.load, binary=True)(data)
         result = func([1, 2, "a", "b"])
         cache._manifest.write()
@@ -56,7 +52,6 @@ class CacheTest(unittest.TestCase):
     def test_memory_serialize_arguments(self):
         """Check custom functions for argument serialization."""
 
-        logging.info("starting argument serialization test")
         func = cache(persist=False, serialize=lambda argument: str(hash(argument)))(data)
         result = func("Hello, world!")
         key = "tests.data({})".format(hash("Hello, world!"))
@@ -65,7 +60,6 @@ class CacheTest(unittest.TestCase):
     def test_memory_expiration(self):
         """Test whether expiration works."""
 
-        logging.info("starting expiration cache test")
         func = cache(persist=False, expiration=2)(data)
         result = func()
         time.sleep(1)
@@ -76,7 +70,6 @@ class CacheTest(unittest.TestCase):
     def test_persistent_file(self):
         """Check if file names are stored correctly."""
 
-        logging.info("starting file name cache test")
         func = cache(persist=True, file=lambda argument: str(argument), extension=".txt")(data)
         func("hello")
         self.assertTrue(cache._files._data.joinpath("hello.txt").exists())
@@ -84,7 +77,6 @@ class CacheTest(unittest.TestCase):
     def test_store_retrieve(self):
         """Check the store and retrieve overrides."""
 
-        logging.info("starting store and retrieve test")
         func = cache(persist=True, store=json.dump, retrieve=json.load, extension=".json")(data)
         func({"number": 1})
         cache._manifest.write()
